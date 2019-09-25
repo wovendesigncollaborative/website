b80ded1ece0521ea10b74fc6f9589fd3b63977b8
paul_dejean <paul.dejean@non.se.com>
2019-09-26 00:42:51 +0530

    Animation is done

---
+++ src/app/modules/what-we-do/animations/animation.ts
@@ -0,0 +1,63 @@
+import { animate, state, style, transition, trigger } from '@angular/animations'
+
+export const FALL = trigger('fall', [
+  state('start', style({
+    flex: "0 1 0",
+  })),
+  state('end', style({
+    flex: "1 1 0",
+  })),
+  transition('start => end', [
+    animate("1s ease-in-out")
+  ]),
+])
+
+export const SHRINK =
+  trigger('shrink', [
+    state('start', style({
+      width: "200px"
+    })),
+    state('end', style({
+      width: "100px",
+    })),
+    transition('start => end', [
+      animate("2s ease-in-out")]),
+  ])
+
+export const SPACING =
+  trigger('spacing', [
+    state('start', style({
+      marginRight: "3.5vw",
+      marginLeft: "3.5vw"
+    })),
+    state('end', style({
+      marginRight: "2.5vw",
+      marginLeft: "2.5vw"
+    })),
+    transition('start => end', [
+      animate("2s ease-in-out")]),
+  ])
+
+export const FADING_IN =
+  trigger('fadingIn', [
+    state('start', style({
+      width: 0,
+      height: 0,
+      opacity: 0
+    })),
+    state('end', style({
+      width: "300px",
+      height: "300px",
+      opacity: 1
+    })),
+    transition('start => end', [
+      animate("10ms ease-in-out", style({
+        width: "300px",
+        height: "300px"
+      })),
+      animate("2s ease-in-out", style({ opacity: 1 })),
+    ])
+  ])
+
+
+
--- src/app/modules/what-we-do/what-we-do.component.ts
+++ src/app/modules/what-we-do/what-we-do.component.ts
@@ -1,69 +1,32 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { FADING_IN, FALL, SHRINK, SPACING } from './animations/animation';
 
 @Component({
   selector: 'app-what-we-do',
   templateUrl: './what-we-do.component.html',
   styleUrls: ['./what-we-do.component.scss'],
   animations: [
-    trigger('fall', [
-      state('start', style({
-        flex: "0 1 0",
-      })),
-      state('end', style({
-        flex: "1 1 0",
-      })),
-      transition('start => end', [
-        animate("1s ease-in-out")
-      ]),
-    ]),
-    trigger('shrink', [
-      state('start', style({
-        width: "200px"
-      })),
-      state('end', style({
-        width: "100px"
-      })),
-      transition('start => end', [
-        animate("2s ease-in-out")]),
-    ]),
-    trigger('spacing', [
-      state('start', style({
-        marginRight: "7vw"
-      })),
-      state('end', style({
-        marginRight: "5vw",
-      })),
-      transition('start => end', [
-        animate("2s ease-in-out")]),
-    ]),
-    trigger('imageAnimation', [
-      state('start', style({
-        width: 0,
-        height: 0,
-        opacity: 0
-      })),
-      state('end', style({
-        width: "400px",
-        height: "400px",
-        opacity: 1
-      })),
-      transition('start => end', [
-        animate("10ms ease-in-out", style({
-          width: "400px",
-          height: "400px"
-        })),
-        animate("2s ease-in-out", style({ opacity: 1 })),
-      ])
-    ]),
+    FALL,
+    SHRINK,
+    SPACING,
+    FADING_IN
   ]
 })
 export class WhatWeDoComponent implements OnInit {
 
   imageSrc = 'assets/img/nisha.jpg';
-  falling = true;
-  shrink = true;
-  imageAnimation = true;
+
+  images = [
+    "assets/img/regional-planning.jpg",
+    "assets/img/urban-design.jpg",
+    "assets/img/rural-development.jpg",
+    "assets/img/environmental-planning.jpg",
+    "assets/img/social-development.jpg"
+  ]
+
+  hasClickedOnce = false;
+  fadingIn = false;
+  indexSelected = null;
 
   constructor() { }
 
@@ -71,10 +34,17 @@ export class WhatWeDoComponent implements OnInit {
 
   }
 
-  onImageClick() {
-    this.falling = false;
-    this.shrink = false;
-    setTimeout(() => this.imageAnimation = false, 2000)
+  onImageClick(index) {
+
+    if (!this.hasClickedOnce) {
+      this.hasClickedOnce = true;
+      setTimeout(() => this.fadingIn = true, 2000);
+    }
+
+
+    this.indexSelected = index;
+    this.imageSrc = this.images[index]
+
   }
 
 }
