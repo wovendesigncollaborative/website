ee1af189bc1d7eebce47a140ad8f6a476a2d2506
paul_dejean <paul.dejean@non.se.com>
2020-01-18 17:49:28 +0530

    Mobile version for contact us

--- src/app/modules/contact-us/contact-us.component.ts
+++ src/app/modules/contact-us/contact-us.component.ts
@@ -6,15 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.component.scss'],
 })
 export class ContactUsComponent implements OnInit {
-  public headline = `With a a multi-disciplinary team, and over 9 years of experience in the field,
-  the experts at Woven Design Collective works closely with NGOs,
-  community associations and local groups to analyse and assess the status at the grassroot.
-  We arrive upon contextual solutions that are rooted in multi-disciplinarity and critical thinking.
-  `;
 
-  public secondHeadline = `We engage with our clients in 3 ways depending on requirements and constraints.`;
 
-  constructor() {}
+  constructor() { }
 
-  public ngOnInit() {}
+  public ngOnInit() { }
 }
