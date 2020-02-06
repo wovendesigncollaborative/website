f59b0dfeec3e51d35a1d75799f034d2f9cb90f9b
paul_dejean <paul.dejean@non.se.com>
2020-02-06 22:56:46 +0530

    Added webp and little responsiveness on nisha's page

--- projects/interior-design/src/app/modules/projects/data/projects.ts
+++ projects/interior-design/src/app/modules/projects/data/projects.ts
@@ -10,102 +10,102 @@ const PROJECTS_PHOTO_FOLDER = 'assets/img/projects';
 
 const MADHUKAR: Project = {
     id: '1',
-    src: `${PROJECTS_PHOTO_FOLDER}/madhukar/cover.jpg`,
+    src: `${PROJECTS_PHOTO_FOLDER}/madhukar/cover.webp`,
     name: 'MADHUKAR RESIDENCE',
     slides: [
-        `${PROJECTS_PHOTO_FOLDER}/madhukar/cover.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/madhukar/1.jpg`,
+        `${PROJECTS_PHOTO_FOLDER}/madhukar/cover.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/madhukar/1.webp`,
         `${PROJECTS_PHOTO_FOLDER}/madhukar/T1.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/madhukar/3.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/madhukar/4.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/madhukar/5.jpg`,
+        `${PROJECTS_PHOTO_FOLDER}/madhukar/3.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/madhukar/4.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/madhukar/5.webp`,
         `${PROJECTS_PHOTO_FOLDER}/madhukar/T2.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/madhukar/6.jpg`,
+        `${PROJECTS_PHOTO_FOLDER}/madhukar/6.webp`,
         `${PROJECTS_PHOTO_FOLDER}/madhukar/T3.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/madhukar/7.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/madhukar/9.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/madhukar/10.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/madhukar/11.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/madhukar/12.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/madhukar/13.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/madhukar/14.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/madhukar/15.jpg`,
+        `${PROJECTS_PHOTO_FOLDER}/madhukar/7.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/madhukar/9.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/madhukar/10.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/madhukar/11.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/madhukar/12.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/madhukar/13.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/madhukar/14.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/madhukar/15.webp`,
     ]
 }
 
 const SUVEER: Project = {
     id: '2',
-    src: `${PROJECTS_PHOTO_FOLDER}/suveer/cover.jpg`,
+    src: `${PROJECTS_PHOTO_FOLDER}/suveer/cover.webp`,
     name: 'HEGDE RESIDENCE',
     slides: [
-        `${PROJECTS_PHOTO_FOLDER}/suveer/cover.jpg`,
+        `${PROJECTS_PHOTO_FOLDER}/suveer/cover.webp`,
         `${PROJECTS_PHOTO_FOLDER}/suveer/T1.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/suveer/2.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/suveer/3.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/suveer/4.jpg`,
+        `${PROJECTS_PHOTO_FOLDER}/suveer/2.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/suveer/3.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/suveer/4.webp`,
         `${PROJECTS_PHOTO_FOLDER}/suveer/T2.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/suveer/5.jpg`,
+        `${PROJECTS_PHOTO_FOLDER}/suveer/5.webp`,
         `${PROJECTS_PHOTO_FOLDER}/suveer/T3.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/suveer/6.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/suveer/7.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/suveer/8.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/suveer/9.jpg`,
+        `${PROJECTS_PHOTO_FOLDER}/suveer/6.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/suveer/7.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/suveer/8.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/suveer/9.webp`,
     ]
 }
 
 const KRISHNA: Project = {
     id: '3',
-    src: `${PROJECTS_PHOTO_FOLDER}/krishna/cover.jpg`,
+    src: `${PROJECTS_PHOTO_FOLDER}/krishna/cover.webp`,
     name: 'VAASTHU RESIDENCE',
     slides: [
-        `${PROJECTS_PHOTO_FOLDER}/krishna/1.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/krishna/2.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/krishna/3.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/krishna/T1a.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/krishna/4.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/krishna/T2a.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/krishna/5.jpg`,
+        //`${PROJECTS_PHOTO_FOLDER}/krishna/1.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/krishna/2.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/krishna/3.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/krishna/T1.jpg`,
+        `${PROJECTS_PHOTO_FOLDER}/krishna/4.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/krishna/T2.jpg`,
+        `${PROJECTS_PHOTO_FOLDER}/krishna/5.webp`,
         `${PROJECTS_PHOTO_FOLDER}/krishna/T3.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/krishna/6.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/krishna/7.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/krishna/9.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/krishna/10.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/krishna/11.jpg`,
+        `${PROJECTS_PHOTO_FOLDER}/krishna/6.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/krishna/7.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/krishna/9.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/krishna/10.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/krishna/11.webp`,
     ]
 }
 
 const ALEX: Project = {
     id: '4',
-    src: `${PROJECTS_PHOTO_FOLDER}/alex/cover.jpg`,
+    src: `${PROJECTS_PHOTO_FOLDER}/alex/cover.webp`,
     name: 'HEXAGON HOME',
     slides: [
-        `${PROJECTS_PHOTO_FOLDER}/alex/cover.jpg`,
+        `${PROJECTS_PHOTO_FOLDER}/alex/cover.webp`,
         `${PROJECTS_PHOTO_FOLDER}/alex/T1.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/alex/1.jpg`,
+        `${PROJECTS_PHOTO_FOLDER}/alex/1.webp`,
         `${PROJECTS_PHOTO_FOLDER}/alex/T2.jpg`,
     ]
 }
 
 const GEETHA: Project = {
     id: '5',
-    src: `${PROJECTS_PHOTO_FOLDER}/geetha/cover.jpg`,
+    src: `${PROJECTS_PHOTO_FOLDER}/geetha/cover.webp`,
     name: 'VERNACULAR RESIDENCE',
     slides: [
-        `${PROJECTS_PHOTO_FOLDER}/geetha/cover.jpg`,
+        `${PROJECTS_PHOTO_FOLDER}/geetha/cover.webp`,
         `${PROJECTS_PHOTO_FOLDER}/geetha/T1.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/geetha/2.jpg`,
+        `${PROJECTS_PHOTO_FOLDER}/geetha/2.webp`,
         `${PROJECTS_PHOTO_FOLDER}/geetha/T2.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/geetha/3.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/geetha/4.jpg`,
+        `${PROJECTS_PHOTO_FOLDER}/geetha/3.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/geetha/4.webp`,
     ]
 }
 
 const NARESH: Project = {
     id: '6',
-    src: `${PROJECTS_PHOTO_FOLDER}/naresh/cover.jpg`,
+    src: `${PROJECTS_PHOTO_FOLDER}/naresh/cover.webp`,
     name: 'HOME FOR THE CHOWTAS',
     slides: [
-        `${PROJECTS_PHOTO_FOLDER}/naresh/cover.jpg`,
+        `${PROJECTS_PHOTO_FOLDER}/naresh/cover.webp`,
         `${PROJECTS_PHOTO_FOLDER}/naresh/T1.jpg`,
         `${PROJECTS_PHOTO_FOLDER}/naresh/T2.jpg`,
     ]
@@ -113,33 +113,33 @@ const NARESH: Project = {
 
 const OFFICE: Project = {
     id: '7',
-    src: `${PROJECTS_PHOTO_FOLDER}/office/cover.jpg`,
+    src: `${PROJECTS_PHOTO_FOLDER}/office/cover.webp`,
     name: 'WDC OFFICE',
     slides: [
-        `${PROJECTS_PHOTO_FOLDER}/office/cover.jpg`,
+        `${PROJECTS_PHOTO_FOLDER}/office/cover.webp`,
         `${PROJECTS_PHOTO_FOLDER}/office/T1.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/office/2.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/office/3.jpg`,
+        `${PROJECTS_PHOTO_FOLDER}/office/2.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/office/3.webp`,
         `${PROJECTS_PHOTO_FOLDER}/office/T2.jpg`,
     ]
 }
 
 const TROPICAL: Project = {
     id: '8',
-    src: `${PROJECTS_PHOTO_FOLDER}/tropical/cover.jpg`,
+    src: `${PROJECTS_PHOTO_FOLDER}/tropical/cover.webp`,
     name: 'TROPICAL INTERIORS',
     slides: [
-        `${PROJECTS_PHOTO_FOLDER}/tropical/cover.jpg`,
+        `${PROJECTS_PHOTO_FOLDER}/tropical/cover.webp`,
         `${PROJECTS_PHOTO_FOLDER}/tropical/T1.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/tropical/1.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/tropical/9.jpg`,
+        `${PROJECTS_PHOTO_FOLDER}/tropical/1.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/tropical/9.webp`,
         `${PROJECTS_PHOTO_FOLDER}/tropical/T2.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/tropical/3.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/tropical/4.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/tropical/6.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/tropical/7.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/tropical/8.jpg`,
-        `${PROJECTS_PHOTO_FOLDER}/tropical/2.jpg`,
+        `${PROJECTS_PHOTO_FOLDER}/tropical/3.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/tropical/4.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/tropical/6.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/tropical/7.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/tropical/8.webp`,
+        `${PROJECTS_PHOTO_FOLDER}/tropical/2.webp`,
 
     ]
 }
