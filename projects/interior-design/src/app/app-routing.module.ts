5fecad2bbf9323872ee35073374e6dc29b2f1853
paul_dejean <paul.dejean@non.se.com>
2020-02-11 18:19:36 +0530

    Change preloading strategy for lazy routes

--- projects/interior-design/src/app/app-routing.module.ts
+++ projects/interior-design/src/app/app-routing.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { ContactUsComponent } from './modules/contact-us/contact-us.component';
 import { HomeComponent } from './modules/home/home.component';
 
 const routes: Routes = [
-    //{ path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: '', component: HomeComponent },
     { path: 'projects', loadChildren: () => import('./modules/projects/projects.module').then(m => m.ProjectsModule) },
     { path: 'contacts', component: ContactUsComponent }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
