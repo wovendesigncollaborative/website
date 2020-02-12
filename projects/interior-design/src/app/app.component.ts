ca56913ea6fbafbdd6b541b1e69b65f9b0af7571
paul_dejean <paul.dejean@non.se.com>
2020-02-12 23:15:09 +0530

    Disable animations before loading

--- projects/interior-design/src/app/app.component.ts
+++ projects/interior-design/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  isApplicationLoaded = false;
+  ngAfterViewInit(): void {
+    this.isApplicationLoaded = true;
+  }
 
 }
